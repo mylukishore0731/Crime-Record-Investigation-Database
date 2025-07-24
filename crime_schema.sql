fecace110d15e5dd6d75b73efd0a1c7fd15ec370
mylukishore0731 <kishoredmylu0731@gmail.com>
2025-07-24 18:18:13 +0530

    Update crime_schema.sql

--- crime_schema.sql
+++ crime_schema.sql
@@ -1,36 +1,36 @@
 -- Create Crime Record & Investigation Database schema
 
 CREATE TABLE Officers (
-    OfficerID SERIAL PRIMARY KEY,
+    OfficerID NUMBER(10) PRIMARY KEY,
     Name VARCHAR(100),
     Rank VARCHAR(50),
     Department VARCHAR(100)
 );
 
 CREATE TABLE Cases (
-    CaseID SERIAL PRIMARY KEY,
-    Title TEXT NOT NULL,
+    CaseID NUMBER(50) PRIMARY KEY,
+    Title VARCHAR2(100) NOT NULL,
     Status VARCHAR(20) CHECK (Status IN ('Open', 'Closed', 'Under Investigation')),
     StartDate DATE NOT NULL,
     EndDate DATE,
-    LeadOfficerID INT REFERENCES Officers(OfficerID)
+    FOREIGN KEY OfficerID NUMBER(20) REFERENCES Officers(OfficerID)
 );
 
 CREATE TABLE Suspects (
     SuspectID SERIAL PRIMARY KEY,
     Name VARCHAR(100),
-    Age INT,
+    Age NUMBER(20),
     Gender VARCHAR(10),
-    CaseID INT REFERENCES Cases(CaseID),
-    Arrested BOOLEAN DEFAULT FALSE
+    FOREIGN KEY CaseID NUMBER(50) REFERENCES Cases(CaseID),
+   
 );
 
 CREATE TABLE Evidence (
-    EvidenceID SERIAL PRIMARY KEY,
-    Description TEXT,
+    EvidenceID VARCHAR(50) PRIMARY KEY,
+    Description VARCHAR2(100),
     CollectedDate DATE,
-    CaseID INT REFERENCES Cases(CaseID),
-    UpdatedBy INT REFERENCES Officers(OfficerID)
+    FOREIGN KEY CaseID NUMBER(50) REFERENCES Cases(CaseID),
+    UpdatedBy NUMBER REFERENCES Officers(OfficerID)
 );
 
 -- Indexes
